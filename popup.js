a678ac08c866a6d5282e94e1c07aed70ea2e5d10
Daniel Brakke <brakphoto@gmail.com>
2020-02-15 18:39:44 -0600

    add option to change host if clicking highlighted link

--- popup.js
+++ popup.js
@@ -1,17 +1,29 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    let checkbox = document.getElementById("watch-checkbox");
-    chrome.storage.sync.get(["enabled"], function (result) {
-        if (result.enabled) {
-            checkbox.checked = true;
-        }
-    });
-
-    checkbox.onchange = function () {
-        if (checkbox.checked) {
+    let watchCheckbox = document.getElementById("watch-checkbox");
+    watchCheckbox.onchange = function () {
+        if (watchCheckbox.checked) {
             chrome.storage.sync.set({enabled: true});
         } else {
             chrome.storage.sync.set({enabled: false});
         }
     };
+
+    let changeHostCheckbox = document.getElementById("ask-to-change-host-checkbox");
+    changeHostCheckbox.onchange = function () {
+        if (changeHostCheckbox.checked) {
+            chrome.storage.sync.set({askToChangeHosts: true});
+        } else {
+            chrome.storage.sync.set({askToChangeHosts: false});
+        }
+    };
+
+    chrome.storage.sync.get(["enabled", "askToChangeHosts"], function (result) {
+        if (result.enabled) {
+            watchCheckbox.checked = true;
+        }
+        if (result.askToChangeHosts) {
+            changeHostCheckbox.checked = true;
+        }
+    });
 });
--- script.js
+++ script.js
@@ -8,16 +8,35 @@ const highlightedStyles = {
 };
 const anchorCheckTimeout = 2000;
 
+let askToChangeHosts;
 function highlightAnchors() {
-    const allAnchors = document.getElementsByTagName("a");
-    for (let a of allAnchors) {
-        if (!a.href || a.getAttribute("data-qa-visited") === "true") {
+    const uncheckedAnchors = document.querySelectorAll("a:not([data-qa-visited])");
+    for (let a of uncheckedAnchors) {
+        if (!a.href) {
+            a.setAttribute("data-qa-visited", "true");
+            continue;
+        } else if (a.getAttribute("data-qa-visited") === "true") {
             continue;
         }
         const url = new URL(a.href);
         const isDifferentHost = url.host !== host;
         if (isDifferentHost) {
             Object.assign(a.style, highlightedStyles);
+            a.onclick = function (e) {
+                e.preventDefault();
+                if (window.confirm("Different host, proceed?")) {
+                    if (askToChangeHosts) {
+                        if (window.confirm(`Change host to ${host}?`)) {
+                            url.host = host;
+                            window.location.href = url.toString();
+                        } else {
+                            window.location.href = url.toString();
+                        }
+                    } else {
+                        window.location.href = url.toString();
+                    }
+                }
+            }
         }
         a.setAttribute("data-qa-visited", "true");
     }
@@ -25,21 +44,35 @@ function highlightAnchors() {
 
 let intervalId;
 function startParsingIfEnabled() {
-    chrome.storage.sync.get(['enabled'], function (result) {
-        if (!result.enabled) {
-            clearInterval(intervalId);
-        } else {
+    chrome.storage.sync.get(["enabled", "askToChangeHosts"], function (result) {
+        if (result.enabled) {
             highlightAnchors();
             intervalId = setInterval(highlightAnchors, anchorCheckTimeout);
+        } else {
+            clearInterval(intervalId);
+        }
+        if (result.askToChangeHosts) {
+            askToChangeHosts = true;
+        } else {
+            askToChangeHosts = false;
         }
     });
 }
 
 chrome.storage.onChanged.addListener(function (changes) {
-    if (changes.enabled.newValue) {
-        startParsingIfEnabled();
-    } else {
-        clearInterval(intervalId);
+    if (changes.enabled !== undefined) {
+        if (changes.enabled.newValue) {
+            startParsingIfEnabled();
+        } else {
+            clearInterval(intervalId);
+        }
+    }
+    if (changes.askToChangeHosts !== undefined) {
+        if (changes.askToChangeHosts.newValue) {
+            askToChangeHosts = true;
+        } else {
+            askToChangeHosts = false;
+        }
     }
 });
 
